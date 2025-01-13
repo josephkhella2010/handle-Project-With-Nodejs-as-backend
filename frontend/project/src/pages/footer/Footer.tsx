a46582cded1a0937b15b3ff1313d7eafc5eb3207
joseph khella <josephkhella2010@gmail.com>
2025-01-13 09:02:51 +0100

    create footer

--- frontend/project/src/pages/footer/Footer.tsx
+++ frontend/project/src/pages/footer/Footer.tsx
@@ -1,15 +1,31 @@
 import React from "react";
 import styles from "./footer.module.css";
+import { FaFacebook, FaInstagram, FaLinkedin, FaYoutube } from "react-icons/fa";
 
 export default function Footer() {
   return (
     <div className={styles.footerMainWrapper}>
       <div className={styles.footerWrapper}>
-        <h1>footer</h1>
-        <h1>footer</h1>
-        <h1>footer</h1>
-        <h1>footer</h1>
-        <h1>footer</h1>
+        <h1>shopping gjort enkelt.</h1>
+        <div className={styles.bottomSection}>
+          <div className={styles.linksSection}>
+            <h4>Länkar</h4>
+            <ul className={styles.linksList}>
+              <a href="/">Home</a>
+              <a href="/product">Product</a>
+              <a href="/login">Login Eller Skapa Konto</a>
+            </ul>
+          </div>
+          <div className={styles.socialSection}>
+            <h4>Sociala medier</h4>
+            <div className={styles.socialIcons}>
+              <FaFacebook className={styles.icons} />
+              <FaInstagram className={styles.icons} />
+              <FaYoutube className={styles.icons} />
+              <FaLinkedin className={styles.icons} />
+            </div>
+          </div>
+        </div>
       </div>
     </div>
   );
