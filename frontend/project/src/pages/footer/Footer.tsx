a90d2052384391840efacb4a5f3231607269e982
joseph khella <josephkhella2010@gmail.com>
2025-01-13 09:20:42 +0100

    linked icons

--- frontend/project/src/pages/footer/Footer.tsx
+++ frontend/project/src/pages/footer/Footer.tsx
@@ -19,10 +19,30 @@ export default function Footer() {
           <div className={styles.socialSection}>
             <h4>Sociala medier</h4>
             <div className={styles.socialIcons}>
-              <FaFacebook className={styles.icons} />
-              <FaInstagram className={styles.icons} />
-              <FaYoutube className={styles.icons} />
-              <FaLinkedin className={styles.icons} />
+              <a
+                href="http://www.facebook.com"
+                target="_blank"
+                rel="noopener noreferrer">
+                <FaFacebook className={styles.icons} title="Facebook" />
+              </a>
+              <a
+                href="http://www.instagram.com"
+                target="_blank"
+                rel="noopener noreferrer">
+                <FaInstagram className={styles.icons} title="Instagram" />
+              </a>
+              <a
+                href="http://www.youtube.com"
+                target="_blank"
+                rel="noopener noreferrer">
+                <FaYoutube className={styles.icons} title="YouTube" />
+              </a>
+              <a
+                href="http://www.linkedin.com"
+                target="_blank"
+                rel="noopener noreferrer">
+                <FaLinkedin className={styles.icons} title="LinkedIn" />
+              </a>
             </div>
           </div>
         </div>
